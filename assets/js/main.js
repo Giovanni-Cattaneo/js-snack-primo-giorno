f339e80beea964f236728306547a0ba9d428f69c
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-16 15:28:00 +0100

    completed excercise 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,13 @@ console.log(PI);
 
 
 // Es 2/C
-let radius = 8;
+let radius = 8; //number
 console.log(radius * 2 * PI);
-let circle = (radius * 2 * PI);
+let circle = (radius * 2 * PI); //number
 console.log(circle);
+
+
+// Es 3
+let name = "Carlo" // String
+name = "Marco" // String
+console.log(name);
