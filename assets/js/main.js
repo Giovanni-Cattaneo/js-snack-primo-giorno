e3fe104b6a93f7446b79d5e0c5fa375b97866b5b
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-16 15:18:22 +0100

    done es 2/b start 2/c

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,19 @@
 // Es 1
 
-let number = 100
+let number = 100; //number
 
 console.log(number);
 
 // Es 2
 
-const PI = 3.14
+const PI = 3.14; //number
 
 console.log(PI);
+
+// Es 2/B
+/*const PI = 3.1416; cambiare valore ad una const provoca errore cambia con un let se necessario */
+
+
+// Es 2/C
+let radius = 8
+console.log(radius * 2 * PI);
